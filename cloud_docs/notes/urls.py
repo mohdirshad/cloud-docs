53fc46c712f01e0cfb3b70a4d94b0f2168778b4c
Irshad <irshad@irshad-moocs.(none)>
2014-01-07 17:47:56 +0530

    create complete

--- cloud_docs/notes/urls.py
+++ cloud_docs/notes/urls.py
@@ -3,7 +3,7 @@ from notes.views import *
 
 
 urlpatterns = patterns('',
-     url(r'^$', Note.as_view(), name='notelist'),
-     url(r'^add/$', Create_notes.as_view(), name='create'),
+     url(r'^$', Notes.as_view(), name='notelist'),
+     url(r'^add/$', CreateNote.as_view(), name='create'),
 
 )
--- cloud_docs/notes/views.py
+++ cloud_docs/notes/views.py
@@ -8,25 +8,28 @@ from notes.forms import *
 
 class Notes(View):
     """class to fetch and store Notes"""
-    template_name="notes.html" 
+    template_name="notes.html"
+    
 
     def get(self,request):
-	notes=Note.objects.all()
-	return render_to_response(template_name,{'notes':notes} , ci)
+	ci= RequestContext(request)
+	notes=Note.objects.filter(user=request.user)
+	return render_to_response(self.template_name,{'notes':notes} ,ci)
 
 
 
-class Create_notes(View):
+class CreateNote(View):
     """create notes"""
-    form = Noteform()
+    noteform = Noteform
     template_name="createnote.html"
     
     def get(self, request, *args, **kwargs):
-	form = self.form
-	render_to_response(template_name , {'form':form}, ci)
+	ci=RequestContext(request)
+	form = self.noteform
+	return render_to_response(self.template_name , {'form':form}, ci)
     
     def post(self , request , *args, **kwargs ):
-        form = self.form(request.POST)
+        form = self.noteform(request.POST)
 	if form.is_valid():
 		note=form.save(commit=False)
 		note.user=request.user
